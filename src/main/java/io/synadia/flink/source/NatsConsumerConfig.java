3aa889f4b132b854bf956d959910a2362b9c6468
somratdutta <duttasomratand@gmail.com>
2023-11-29 15:20:57 +0530

    Add extra line after license header Add extra line after license header

--- src/main/java/io/synadia/flink/source/NatsConsumerConfig.java
+++ src/main/java/io/synadia/flink/source/NatsConsumerConfig.java
@@ -1,5 +1,6 @@
 // Copyright (c) 2023 Synadia Communications Inc. All Rights Reserved.
 // See LICENSE and NOTICE file for details.
+
 package io.synadia.flink.source;
 import java.io.Serializable;
 
