0d56866542a340c30ee23ac99bf5fd363506b244
scottf <scottfauerbach@gmail.com>
2024-12-19 16:30:00 -0500

    Finish JetStream Sink Writer and updated licenses

--- src/main/java/io/synadia/flink/v0/sink/NatsJetStreamSinkBuilder.java
+++ src/main/java/io/synadia/flink/v0/sink/NatsJetStreamSinkBuilder.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2023-2024 Synadia Communications Inc. All Rights Reserved.
+// Copyright (c) 2024 Synadia Communications Inc. All Rights Reserved.
 // See LICENSE and NOTICE file for details. 
 
 package io.synadia.flink.v0.sink;
